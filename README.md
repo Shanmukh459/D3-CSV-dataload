a8b681e6c1787eaeb80f39c8803a59d57744422f
Shanmukh459 <saikrrish459@gmail.com>
2024-04-19 12:24:12 -0700

    working code-need to refactor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# D3 for CSV Data Loading
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { csv, csvFormat } from 'd3';
+
+const csvUrl = 'https://gist.githubusercontent.com/Shanmukh459/cc27c72a6bd5e5d41618edf3eb91612a/raw/cssNamedColors.csv';
+
+const message = (data) => {
+  let message = '';
+  message += data.length + ' rows\n';
+  message += data.columns.length + ' columns\n';
+  message += Math.round(csvFormat(data).length / 1024) + ' KB';
+  return message
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    csv(csvUrl).then(setData);
+  }, [])
 
   return (
-    <h1>Hello!</h1>
+    <pre>Data is {data ? message(data) : 'loading'}</pre>
   )
 }
 
