2973f6a88f4f214b8972f7decbd1ac5a01371ba0
Shanmukh459 <saikrrish459@gmail.com>
2024-04-19 17:03:07 -0700

    pie chart for the colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from 'react'
-import { csv } from 'd3';
+import { arc, csv } from 'd3';
 // import { message } from './message';
 
 const csvUrl = 'https://gist.githubusercontent.com/Shanmukh459/cc27c72a6bd5e5d41618edf3eb91612a/raw/cssNamedColors.csv';
+const height = window.screen.height
+const width = window.screen.width
+const centerX = width / 2
+const centerY = height / 2
+
+const pieArc = arc()
+  .innerRadius(0)
+  .outerRadius(width)
 
 function App() {
   const [data, setData] = useState(null)
@@ -11,9 +19,28 @@ function App() {
     csv(csvUrl).then(setData);
   }, [])
 
+  if(!data){
+    return <pre>Loading...</pre>
+  }
+
   return (
-    <pre>Data is {data ? message(data) : 'loading'}</pre>
+    <svg height={height} width={width}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        {
+          data.map((d, i) => (
+          <path 
+            fill={d['RGB hex value']} 
+            d={pieArc({
+              startAngle: i/data.length * Math.PI * 2,
+              endAngle: (i+1)/data.length * Math.PI*2
+            })}
+          />
+        ))
+        }
+      </g>  
+    </svg>
   )
+
 }
 
 export default App
